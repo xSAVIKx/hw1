1beed5d5e8ac6b11de31ce1495bad10b3bf25415
Igor Afanasenko <igoraf07@gmail.com>
2016-11-19 21:40:00 +0200

    Task 3-4 done.

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task3.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task3.java
@@ -1,7 +1,9 @@
 package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task3 {
-    public static boolean isBetween(int a, int b, int c) {
-        return false;
+    public static boolean isBetween(int a, int b, int c)
+    {
+        return
+                a<c? b>=a && b<=c? true: false: b<=a && b>=c? true: false; // Test 3 done.
     }
 }
--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task4.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task4.java
@@ -3,7 +3,16 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task4 {
 
-    public static double getSumOfGreatest(double a, double b, double c) {
-        return 0;
+    public static double getSumOfGreatest(double a, double b, double c)
+    {
+        if (a > b && b > c)
+        {
+            return a + b;
+        }
+        if (a > b && b < c)
+        {
+            return a + c;
+        }
+        return b + c; //Test 4 done.
     }
 }
