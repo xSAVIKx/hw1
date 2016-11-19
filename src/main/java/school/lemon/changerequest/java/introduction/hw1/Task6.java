171ba71bb37a69b2620d925ce3e9c6d16361a689
Igor Afanasenko <igoraf07@gmail.com>
2016-11-19 21:44:46 +0200

    Task 6 done.

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task6.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task6.java
@@ -1,11 +1,15 @@
 package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task6 {
-    public static double calculateS(double x) {
-        return 0d;
-    }
+    public static double calculateS(double x)
+    {
+        return
+                1+x+Math.pow(x,2)/(1*2)+Math.pow(x,3)/(1*2*3)+Math.pow(x,4)/(1*2*3*4);
+            }
 
-    public static double calculateZ(double x, double y) {
-        return 0d;
+    public static double calculateZ(double x, double y)
+    {
+        return
+                Math.sin(x*x*x)+Math.pow(Math.cos(y),2); // Test 6 done.
     }
 }
