91abe8cca933acec1f65721a5f4f2113aa83b555
Igor Afanasenko <igoraf07@gmail.com>
2016-11-19 21:52:23 +0200

    Task 10 done.

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task10.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task10.java
@@ -3,7 +3,24 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task10 {
 
-    public static boolean containDigitTwo(int n) {
-        return false;
+    public static boolean containDigitTwo(int n)
+    {
+        int j=2;
+        for (int i = 1; i < j; i++)
+        {
+            float Num = n /10f;
+            int Raz=(int)((Num-(int)Num)*10);
+            if (Raz == 2)
+            {
+                return true;
+            }
+            if ((int)Num == 0)
+            {
+                break;
+            }
+            n=(int)Num;
+            j++;
+        }
+        return false; // Test 10 done.
     }
 }
