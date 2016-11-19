f96904e0f42bb5048ab6a2b71a3c518a28141760
Igor Afanasenko <igoraf07@gmail.com>
2016-11-19 21:41:48 +0200

    Task 5 done.

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task5.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task5.java
@@ -2,11 +2,14 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 
 public class Task5 {
-    public static double calculateA(double x, double y, double z) {
-        return 0;
+    public static double calculateA(double x, double y, double z)
+    {
+        return
+                2*(Math.sin(x-(Math.PI)/6))*calculateB(z)/(0.5+(Math.sin(y))*(Math.sin(y)));
     }
 
-    private static double calculateB(double z) {
-        return 0;
+    private static double calculateB(double z)
+    {
+        return 1+z*z/(3+(z*z)/5); // Test 5 done.
     }
 }
