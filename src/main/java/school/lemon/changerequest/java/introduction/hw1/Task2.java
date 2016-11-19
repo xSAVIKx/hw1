af3c2a3c5708a108e0d71ffff89a474c5845eafc
Ihor Kandaurov <heroy44@gmail.com>
2016-11-19 12:54:24 +0200

    Task 1, 2 done

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task2.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task2.java
@@ -3,6 +3,10 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task2 {
     public static int getMeters(int centimeters) {
-        return 0;
+        double b = .01;//тут мы вносим дабл 0.01
+        return (int) (centimeters * b);//не забываем поставить вывод через int
+    }
+    public static void main (String args[]){
+        System.out.println(Task2.getMeters(1500)); //проверочка
     }
 }
