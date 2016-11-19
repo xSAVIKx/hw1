d4ddb2bd699f57e69451c6a1d5894955ae14a84f
Ihor Kandaurov <heroy44@gmail.com>
2016-11-19 13:54:23 +0200

    Task 3 done

--- src/test/groovy/school/lemon/changerequest/java/introduction/hw1/Task3Specification.groovy
+++ src/test/groovy/school/lemon/changerequest/java/introduction/hw1/Task3Specification.groovy
@@ -16,5 +16,6 @@ class Task3Specification extends Specification {
         6   | 6   | 6  || true
         -1  | -2  | 0  || false
         -28 | -26 | 26 || true
+        51  | 128 | 1  || true
     }
 }
