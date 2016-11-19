4ee76d3c3484f5f6455d90b959172a89a3ed4443
Lera Kulikova <eklera0506@gmail.com>
2016-11-19 22:32:03 +0200

    change files

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task1.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task1.java
@@ -3,6 +3,12 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task1 {
     public static double calculateP(double a) {
-        return 0;
+        double P;
+        return  P = 4 * a;
     }
+
+    public static void main(String[] args) {
+        System.out.println(calculateP(5.1));
+    }
+
 }
--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task3.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task3.java
@@ -2,6 +2,13 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task3 {
     public static boolean isBetween(int a, int b, int c) {
+        if (a < b && b < c){
+            return true;
+        }
         return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isBetween(-1, 0, 50));
     }
 }
