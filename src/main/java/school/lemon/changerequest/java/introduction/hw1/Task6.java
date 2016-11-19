363efae3f0da0d5477bf6239507d88d8d22dd110
Ihor Kandaurov <heroy44@gmail.com>
2016-11-19 15:34:22 +0200

    Task 6 done

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task6.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task6.java
@@ -1,11 +1,19 @@
 package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task6 {
+    private static int factorial(int n) {
+        if (n == 0) return 1;
+        return n * factorial(n - 1);
+    } //мастерский метод для нахождения факториала
+
     public static double calculateS(double x) {
-        return 0d;
+
+        return (1+x+ Math.pow(x, 2)/factorial(2)+Math.pow(x, 3)/factorial(3)+Math.pow(x, 4)/factorial(4));
     }
 
     public static double calculateZ(double x, double y) {
-        return 0d;
+
+
+        return (Math.sin(Math.pow(x, 3))+Math.pow(Math.cos(y), 2));
     }
 }
