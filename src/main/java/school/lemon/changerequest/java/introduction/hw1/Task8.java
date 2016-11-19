8a1f354bb610c7179315b6c2a9e26943245140c3
Ihor Kandaurov <heroy44@gmail.com>
2016-11-19 17:32:55 +0200

    Task 8 done

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task8.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task8.java
@@ -1,7 +1,11 @@
 package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task8 {
-    public static int calculateSum(int N) {
-        return 0;
-    }
-}
+    public static double calculateSum(double N) {
+        double sum=0;
+        for(int i = 0;i<=N;i++) {
+            sum+=Math.pow((N+i),2);//hard place
+        }
+        return sum;
+    }}
+
