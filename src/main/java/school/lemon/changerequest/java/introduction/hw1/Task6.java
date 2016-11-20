33ba38a4e64be8327e5d02d6977c85937bba7e2e
dianasinenchenko <diana.sinenchenko@gmail.com>
2016-11-20 22:32:48 +0200

    6 done

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task6.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task6.java
@@ -1,11 +1,25 @@
 package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task6 {
-    public static double calculateS(double x) {
-        return 0d;
+
+   public static int fact(int n) {
+        int result;
+
+        if(n==1) return 1;
+        result = fact(n-1) * n;
+        return result;
     }
 
+
+    public static double calculateS(double x) {
+
+        return  1 + x + Math.pow(x,2) / fact(2) + Math.pow(x,3) / fact(3) + Math.pow(x,4) / fact(4) ;
+    }
+
+
     public static double calculateZ(double x, double y) {
-        return 0d;
+
+        return (Math.sin(Math.pow(x, 3))+Math.pow(Math.cos(y), 2));
+
     }
 }
