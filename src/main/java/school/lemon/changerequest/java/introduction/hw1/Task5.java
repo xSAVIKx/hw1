781abde86bfd529eb0409cd9d9b9ae7c79ab7b23
Yaroslav Pavlinskiy <yarbezl@gmail.com>
2016-11-20 22:48:52 +0200

    Tasks 5 and 6 done,

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task5.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task5.java
@@ -3,10 +3,15 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task5 {
     public static double calculateA(double x, double y, double z) {
-        return 0;
+        double a;
+        a = (2 * Math.sin(x-(Math.PI)/6)*calculateB(z));
+        a = a / (0.5 +Math.pow(Math.sin(y),2));
+        return a;
     }
 
     private static double calculateB(double z) {
-        return 0;
+       return 1 +((z*z)/(3+(z*z)/5));
     }
+
+
 }
--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task6.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task6.java
@@ -2,10 +2,18 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task6 {
     public static double calculateS(double x) {
-        return 0d;
+        double s;
+        s = 1 + x;
+        s += (Math.pow(x,2))/2;
+        s+= (Math.pow(x,3))/6;
+        s+=(Math.pow(x,4))/24;
+
+        return s;
     }
 
     public static double calculateZ(double x, double y) {
-        return 0d;
+        double z = Math.pow(Math.cos(y),2);
+        z += Math.sin(Math.pow(x,3));
+        return z;
     }
 }
