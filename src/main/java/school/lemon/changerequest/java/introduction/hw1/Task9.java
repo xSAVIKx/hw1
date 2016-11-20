989fd8b29ba1da29a4f8676f99572d57cf02eb97
Igor Afanasenko <igoraf07@gmail.com>
2016-11-20 17:48:11 +0200

    Fixed Task 9.

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task9.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task9.java
@@ -2,11 +2,13 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task9 {
 
-    public static boolean isPowerOfThree(int n)
-    {
-        double Rezult=Math.log(n)*(int)(Math.log(3))/((int)(Math.log(n))*(Math.log(3)));
-        if (Rezult==1)
-        {return true;}
-        return false; // Test 9 done.
+    public static boolean isPowerOfThree(int n) {
+        if (n == 0) {
+            return false;
+        }
+        while (n % 3 == 0) {
+            n = n / 3;
+        }
+        return n == 1; // Test 9 done.
     }
 }
