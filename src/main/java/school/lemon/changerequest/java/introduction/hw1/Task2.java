52b490e4e29c137fb8991d2f707b6e3f10c1962c
dianasinenchenko <diana.sinenchenko@gmail.com>
2016-11-20 19:22:48 +0200

    2 done

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task2.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task2.java
@@ -2,7 +2,21 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 
 public class Task2 {
-    public static int getMeters(int centimeters) {
-        return 0;
+    public static int getMeters(int centimeters)
+
+    {
+        return centimeters / 100;
+
+
     }
+
+        public static void main (String [] args) {
+
+            // test
+            System.out.println(Task2.getMeters(334));
+
+
+
+        }
+
 }
