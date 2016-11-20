2c74f9644af5665d71d731af66a5b05c97056f3b
Yaroslav Pavlinskiy <yarbezl@gmail.com>
2016-11-20 22:10:37 +0200

    Task 4 done.

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task4.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task4.java
@@ -4,6 +4,8 @@ package school.lemon.changerequest.java.introduction.hw1;
 public class Task4 {
 
     public static double getSumOfGreatest(double a, double b, double c) {
-        return 0;
+        double m1 = Math.max(a,b);
+        double m2 = Math.max(b,c);
+        return m1+m2;
     }
 }
