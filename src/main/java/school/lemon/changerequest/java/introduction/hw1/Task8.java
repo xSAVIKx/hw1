95b806ae1951ed48d4818779d7bee218907c2a76
dianasinenchenko <diana.sinenchenko@gmail.com>
2016-11-20 23:34:20 +0200

    8 done

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task8.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task8.java
@@ -2,6 +2,14 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task8 {
     public static int calculateSum(int N) {
-        return 0;
+        int Sum = 0;
+
+        for (int n = 0; n <= N; n++)
+        {
+
+            Sum += Math.pow((n+N),2);
+        }
+
+        return Sum;
     }
 }
