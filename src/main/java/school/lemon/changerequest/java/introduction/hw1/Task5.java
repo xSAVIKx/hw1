c3690bf2a5a5706d4dfef3b77231c60e6e74b150
Ihor Kandaurov <heroy44@gmail.com>
2016-11-20 11:47:55 +0200

    Task 5, 8 done

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task5.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task5.java
@@ -5,14 +5,14 @@ import static java.lang.Math.*;
 
 public class Task5 {
     public static double calculateA(double x, double y, double z) {
-        return ((2 * sin(x - (PI / 6)) * calculateB(z)) / ((1 / 2) + pow(sin(y), 2)));
+        return 2*(Math.sin(x-(Math.PI)/6))*calculateB(z)/(0.5+(Math.sin(y))*(Math.sin(y)));
     }
 
     private static double calculateB(double z) {
-        return (1+((pow(z, 2))/(3+((pow(z, 2)/5)))));
+        return 1+z*z/(3+(z*z)/5);
     }
     public static void main (String args[]){
-        System.out.println(Task5.calculateA(1.45, 1.22, 3.5)); // корче считает не правильно, а все записано вроде правильно
+        System.out.println(Task5.calculateA(1.45, 1.22, 3.5)); // works, done
     }
 
 }
--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task8.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task8.java
@@ -4,7 +4,8 @@ public class Task8 {
     public static double calculateSum(double N) {
         double sum=0;
         for(int i = 0;i<=N;i++) {
-            sum+=Math.pow((N+i),2);//hard place
+            sum+=Math.pow((N+i),2);
+            //hard place
         }
         return sum;
     }}
