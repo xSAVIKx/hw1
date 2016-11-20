89417dc464973796ea3f93bbeeeb5bfe63b795fb
dianasinenchenko <diana.sinenchenko@gmail.com>
2016-11-20 19:47:42 +0200

    3 done

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task3.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task3.java
@@ -2,6 +2,9 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task3 {
     public static boolean isBetween(int a, int b, int c) {
-        return false;
+
+       if ( (a >= b) && (b >= c ) || (a <= b) && (b <= c))  return true;
+
+        else return false;
     }
 }
