6e531620d3f3a2a1ac7f54a07ca08327a2cb1d25
Igor Afanasenko <igoraf07@gmail.com>
2016-11-20 13:00:29 +0200

    Fix the Task 10.

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task10.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task10.java
@@ -3,22 +3,17 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task10 {
 
-    public static boolean containDigitTwo(int n)
-    {
-        int j=2;
-        for (int i = 1; i < j; i++)
-        {
-            float Num = n /10f;
-            int Raz=(int)((Num-(int)Num)*10);
-            if (Raz == 2)
-            {
+    public static boolean containDigitTwo(int n) {
+        int j = 2;
+        for (int i = 1; i < j; i++) {
+            int raz = n % 10;
+            if (raz == 2) {
                 return true;
             }
-            if ((int)Num == 0)
-            {
+            if ( n / 10 == 0) {
                 break;
             }
-            n=(int)Num;
+            n = n / 10;
             j++;
         }
         return false; // Test 10 done.
