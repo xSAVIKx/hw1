7388c2f5cfdeffa8e4d13a35b983041b246bb1aa
Ihor <heroy44@gmail.com>
2016-11-20 21:38:48 +0200

    Update Task2.java

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task2.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task2.java
@@ -4,7 +4,7 @@ package school.lemon.changerequest.java.introduction.hw1;
 public class Task2 {
     public static int getMeters(int centimeters) {
 
-        return (centimeters/100);//не забываем поставить вывод через int
+        return centimeters/100;
     }
     public static void main (String args[]){
         System.out.println(Task2.getMeters(1500)); //проверочка
