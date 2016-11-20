66a234d93b8fbea68a6ff574b03a3b4786d500df
Yaroslav Pavlinskiy <yarbezl@gmail.com>
2016-11-20 23:51:40 +0200

    Tasks 7 and 8 done,

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task7.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task7.java
@@ -2,7 +2,35 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 
 public class Task7 {
-    public static String getDayTitle(int dayIndex) {
-        return "";
+    public static String getDayTitle(int dayIndex)
+    { String dayToString;
+
+        switch (dayIndex)
+        {
+            case 1:
+                dayToString = "Monday";
+                break;
+            case 2:
+                dayToString = "Tuesday";
+                break;
+            case 3:
+                dayToString = "Wednesday";
+                break;
+            case 4:
+                dayToString = "Thursday";
+                break;
+            case 5:
+                dayToString = "Friday";
+                break;
+            case 6:
+                dayToString = "Saturday";
+                break;
+            case 7:
+                dayToString = "Sunday";
+                break;
+            default: dayToString = "error";
+                break;
+        }
+        return dayToString;
     }
 }
--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task8.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task8.java
@@ -2,6 +2,13 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task8 {
     public static int calculateSum(int N) {
-        return 0;
+        int sum=0;
+        for(int i=0;(N+i)<=2*N;i++)
+        {
+            sum+= Math.pow((N+i),2);
+        }
+        return sum;
     }
+
+
 }
