0e9c12ab5f0779b01566862cb24e3318bedfa61d
dianasinenchenko <diana.sinenchenko@gmail.com>
2016-11-21 01:18:52 +0200

    9 not all (only true  )

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task9.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task9.java
@@ -3,6 +3,17 @@ package school.lemon.changerequest.java.introduction.hw1;
 public class Task9 {
 
     public static boolean isPowerOfThree(int n) {
-        return false;
-    }
+
+        int i;
+        {
+            do {
+                i = n / 3;
+
+            }
+            while (i % 3 == 1);
+
+            return true;
+
+        }
+           }
 }
