592851c0da57ae1872a552013937fe9e9e34aad0
Yaroslav Pavlinskiy <yarbezl@gmail.com>
2016-11-20 23:57:37 +0200

    Task 10 done.

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task10.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task10.java
@@ -4,6 +4,12 @@ package school.lemon.changerequest.java.introduction.hw1;
 public class Task10 {
 
     public static boolean containDigitTwo(int n) {
+        while(n >= 2)
+        {
+            n /=10;
+            if(n % 10 == 2)
+                return true;
+        }
         return false;
     }
 }
