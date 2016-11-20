1b0d4141b47a59f6643aed64319e82b319cd7950
Ihor Kandaurov <heroy44@gmail.com>
2016-11-20 15:35:02 +0200

    10

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task10.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task10.java
@@ -4,21 +4,17 @@ package school.lemon.changerequest.java.introduction.hw1;
 public class Task10 {
 
     public static boolean containDigitTwo(int n) {
-//        int c = 2;
-//        for (int i = 1; i < c; i++) {
-//            int find = n % 10;
-//            if (find == 2) return true;
-//            if ((n / 10) == 0) break;
-//            n /= 10;
-//            c += 1;
-//        }
-//        return false;
-// не универсален
-        return String.valueOf(n).contains("2"); //переходит к String, но зато пишется одной строкой и выполняет задание
-    }
-
-
+        int c=2;
+        for (int i = 1; i < c; i++) {
+            int find = n % 10;
+            if (find == 2) return true;
+            if ((n / 10) == 0) break;
+            n /= 10;
+            c += 1;
         }
+        return false;
+    }}
+
 
 
 
