fb3df345ff4ce5588ee53315417c4d3d692a5b06
Igor Afanasenko <igoraf07@gmail.com>
2016-11-20 16:42:14 +0200

    Fixed Task 10.

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task10.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task10.java
@@ -4,12 +4,9 @@ package school.lemon.changerequest.java.introduction.hw1;
 public class Task10 {
 
     public static boolean containDigitTwo(int n) {
-        while (true) {
+        while (n != 0) {
             if (n % 10 == 2) {
                 return true;
-            }
-            if (n / 10 == 0) {
-                break;
             }
             n = n / 10;
         }
