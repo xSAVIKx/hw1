b7c2298d0248d8fa897f242c7b33e29c4b328d10
Yaroslav Pavlinskiy <yarbezl@gmail.com>
2016-11-20 23:55:28 +0200

    Task 9 done.

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task9.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task9.java
@@ -2,7 +2,16 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task9 {
 
-    public static boolean isPowerOfThree(int n) {
-        return false;
+    public static boolean isPowerOfThree(int n)
+    {
+        if (n < 1) {
+            return false;
+        }
+
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+
+        return n == 1;
     }
 }
