02de849dd1478bf46fcffc1e24d3566719a6167a
dianasinenchenko <diana.sinenchenko@gmail.com>
2016-11-21 13:27:26 +0200

    10 i can't find mistake

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task10.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task10.java
@@ -4,6 +4,20 @@ package school.lemon.changerequest.java.introduction.hw1;
 public class Task10 {
 
     public static boolean containDigitTwo(int n) {
-        return false;
+        int i;
+        {
+            do {
+
+                i = n % 10;
+                if (i==2)  return true;
+                n=n/10;
+
+            }
+            while (n < 1);
+
+
+        }
+
+       return false;
     }
 }
