01c3502fde6f8e5ff22bfd483de0779677f6820d
irka <irina_eremenko@ukr.net>
2016-11-22 23:46:26 +0200

    Task4 done.

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task4.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task4.java
@@ -4,6 +4,16 @@ package school.lemon.changerequest.java.introduction.hw1;
 public class Task4 {
 
     public static double getSumOfGreatest(double a, double b, double c) {
-        return 0;
+        if (a<b && b<c) return (b+c);
+        if (a>=b && b>=c) return (a+b);
+        else
+            return (a+c);
+    }
+    public static void main (String[] args) {
+        System.out.println(getSumOfGreatest(1, 2, 3));
+        System.out.println(getSumOfGreatest(5, 4, -1));
+        System.out.println(getSumOfGreatest(6, 6, 6));
+        System.out.println(getSumOfGreatest(-1, -2, 0));
+        System.out.println(getSumOfGreatest(-28, -26, 26));
     }
 }
