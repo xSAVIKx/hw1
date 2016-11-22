8702788f1884f0c966f319b5371b1430eb6311c6
irka <irina_eremenko@ukr.net>
2016-11-22 23:23:52 +0200

    Task3 done.

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task3.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task3.java
@@ -2,6 +2,15 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task3 {
     public static boolean isBetween(int a, int b, int c) {
-        return false;
+        if (a>=b && b>=c || a<b && b<c || a>c && b<c) return true;
+        else
+            return false;
+    }
+    public static void main (String[] args){
+        System.out.println(Task3.isBetween(1,2,3));
+        System.out.println(Task3.isBetween(5,4,-1));
+        System.out.println(Task3.isBetween(6,6,6));
+        System.out.println(Task3.isBetween(-1,-2,0));
+        System.out.println(Task3.isBetween(-28,-26,26));
     }
 }
