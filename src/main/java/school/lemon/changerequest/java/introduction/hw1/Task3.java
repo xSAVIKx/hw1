0095b9a1b8e09b9aa932077a70da25d00d221f72
dianasinenchenko <diana.sinenchenko@gmail.com>
2016-11-23 23:41:12 +0200

    3 done changed (without if)

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task3.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task3.java
@@ -3,8 +3,7 @@ package school.lemon.changerequest.java.introduction.hw1;
 public class Task3 {
     public static boolean isBetween(int a, int b, int c) {
 
-       if ( (a >= b) && (b >= c ) || (a <= b) && (b <= c))  return true;
+        return ((a >= b) && (b >= c) || (a <= b) && (b <= c));
 
-        else return false;
     }
 }
