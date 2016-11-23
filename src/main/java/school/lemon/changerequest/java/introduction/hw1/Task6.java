2deceeeb4fd8f97133ad713117071a59bc52ddeb
dianasinenchenko <diana.sinenchenko@gmail.com>
2016-11-24 00:54:20 +0200

    6 done changed (with loop in calculateS)

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task6.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task6.java
@@ -2,24 +2,32 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task6 {
 
-   public static int fact(int n) {
+    public static int fact(int n) {
         int result;
 
-        if(n==1) return 1;
-        result = fact(n-1) * n;
+        if (n == 1) return 1;
+        result = fact(n - 1) * n;
         return result;
     }
 
 
     public static double calculateS(double x) {
+        double sum = 0;
 
-        return  1 + x + Math.pow(x,2) / fact(2) + Math.pow(x,3) / fact(3) + Math.pow(x,4) / fact(4) ;
+        for (int i = 1; i < 5; i++) {
+
+
+            sum += Math.pow(x, i) / fact(i);
+        }
+
+        return sum;
+
     }
 
 
     public static double calculateZ(double x, double y) {
 
-        return (Math.sin(Math.pow(x, 3))+Math.pow(Math.cos(y), 2));
+        return (Math.sin(Math.pow(x, 3)) + Math.pow(Math.cos(y), 2));
 
     }
 }
