217355e4def421f25cc596561abb812ad9b6eba4
dianasinenchenko <diana.sinenchenko@gmail.com>
2016-11-23 23:20:47 +0200

    7 done changed (with return)

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task7.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task7.java
@@ -4,27 +4,33 @@ package school.lemon.changerequest.java.introduction.hw1;
 public class Task7 {
     public static String getDayTitle(int dayIndex) {
 
-        String day_num;
+        switch (dayIndex) {
+            case 1:
+                return "Monday";
 
-        switch (dayIndex )
-        {
-            case 1 : day_num = "Monday";
-            break;
-            case 2 : day_num = "Tuesday";
-            break;
-            case 3 : day_num = "Wednesday";
-            break;
-            case 4 : day_num = "Thursday";
-            break;
-            case 5 : day_num = "Friday";
-            break;
-            case 6 : day_num = "Saturday";
-            break;
-            case 7 : day_num = "Sunday";
-            break;
-            default: day_num = "error";
-            break;
+            case 2:
+                return "Tuesday";
+
+            case 3:
+                return "Wednesday";
+
+            case 4:
+                return "Thursday";
+
+            case 5:
+                return "Friday";
+
+            case 6:
+                return "Saturday";
+
+            case 7:
+                return "Sunday";
+
+            default:
+                return "error";
+
+
         }
-return day_num;
+
     }
 }
