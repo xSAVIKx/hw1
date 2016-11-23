94b65cdb91774687814ba410a8cc054f656d2dc4
dianasinenchenko <diana.sinenchenko@gmail.com>
2016-11-23 19:18:04 +0200

    10  done changed (use loop while instead loop do while)

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task10.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task10.java
@@ -5,18 +5,16 @@ public class Task10 {
 
     public static boolean containDigitTwo(int n) {
         int i;
-        {
-            do {
 
-                i = n % 10;
-                if (i==2)  return true;
+            while (n>0){
+                i = n%10;
+                if (i==2) return true;
                 n=n/10;
 
             }
-            while (n < 1);
 
 
-        }
+
 
        return false;
     }
