7639e99e39b9f5aa0a79dc65538a10cee451fc36
OlgaBerezhna <berezhna.o.a@gmail.com>
2016-11-26 18:12:19 +0200

    Add files via upload

---
+++ task8.java
@@ -0,0 +1,11 @@
+public class task8{
+    public static int CalcSum(int N)
+    {
+        int result = 0;
+        int i;
+        for (i = 0, i <= N, i++){
+            result = result + Math.pow((N + i), 2);
+            return result
+            }
+        }
+    }
