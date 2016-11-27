3aed8cb801b92fb76ae570e936861fdb23105909
Olga Berezhna <berezhna.o.a@gmail.com>
2016-11-27 20:43:42 +0200

    t10done

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task10.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task10.java
@@ -4,6 +4,13 @@ package school.lemon.changerequest.java.introduction.hw1;
 public class Task10 {
 
     public static boolean containDigitTwo(int n) {
+
+        while (n > 0) {
+            if (n % 10 == 2)
+            if (n / 10 == 10) {
+                return true;
+            }
+        }
         return false;
     }
 }
