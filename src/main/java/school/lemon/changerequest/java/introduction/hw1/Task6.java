8700a086fc10d34a92fd236199dc6c3184d89d93
Olga Berezhna <berezhna.o.a@gmail.com>
2016-11-27 17:59:48 +0200

    t6 done

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task6.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task6.java
@@ -2,10 +2,13 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task6 {
     public static double calculateS(double x) {
-        return 0d;
-    }
+        return (1 + (x * (((Math.pow(x, 2) *x) *x) / (2 * (3*2) * (4*3*2)))));
+
+        }
+
 
     public static double calculateZ(double x, double y) {
-        return 0d;
+
+        return Math.sin(Math.pow(x, 3)) + Math.pow(Math.cos(y), 2);
     }
 }
