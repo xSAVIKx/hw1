37593efc978c548d09eca093712a7c821ea576a9
Olga Berezhna <berezhna.o.a@gmail.com>
2016-11-27 16:52:45 +0200

    t4 done

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task4.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task4.java
@@ -4,6 +4,16 @@ package school.lemon.changerequest.java.introduction.hw1;
 public class Task4 {
 
     public static double getSumOfGreatest(double a, double b, double c) {
-        return 0;
-    }
+            if (a < b && b < c) {
+            return c + b;
+            }
+                 if (a > b && b > c) {
+                    return b + a;
+                 }
+                else {
+                    return a + c;
+                 }
+
+
+        }
 }
