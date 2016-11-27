81b9696e99a3030d00c7a5a4193615821fe234d6
Olga Berezhna <berezhna.o.a@gmail.com>
2016-11-27 19:08:31 +0200

    t8 done

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task3.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task3.java
@@ -3,11 +3,8 @@ package school.lemon.changerequest.java.introduction.hw1;
 public class Task3 {
     public static boolean isBetween(int a, int b, int c) {
 
-        if ((a <= b && b <= c) || (a >= b && b >= c)) {
-            return true;
-        } else {
-            return false;
-        }
+        if ((a <= b && b <= c) || (a >= b && b >= c)) return true;
+        else return false;
     }
 }
 
--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task8.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task8.java
@@ -2,6 +2,10 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task8 {
     public static int calculateSum(int N) {
-        return 0;
+        int result = 0;
+        int i;
+        for (i = N; 2 * N >= i; i++)
+            result = (int) (i + Math.pow((2 * N), 2));
+        return result;
     }
 }
