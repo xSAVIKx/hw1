72a2fb6f50d8f023cb1b52c4bb8fd34ece197b9b
Yaroslav Pavlinskiy <yarbezl@gmail.com>
2016-11-28 18:34:33 +0200

    Fixed 4 Task

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task4.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task4.java
@@ -4,15 +4,13 @@ package school.lemon.changerequest.java.introduction.hw1;
 public class Task4 {
 
     public static double getSumOfGreatest(double a, double b, double c) {
-        if(a > b && b > c)
-            return  a + b;
-        else
-            if(b > a && a > c)
-                return b + a;
-        else
-            if(c > a && a > b)
-            return c + a;
-        else return b + c;
+        if (a > b && b > c) {
+            return a + b;
+        }
+        if (a > b && b < c) {
+            return a + c;
+        }
+        return b + c;
 
     }
 }
