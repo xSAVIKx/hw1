d142eb7a182f23f3b09d3683d47bac5896f59330
Olga Berezhna <berezhna.o.a@gmail.com>
2016-11-28 13:20:50 +0200

    t6_corrected

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task6.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task6.java
@@ -2,10 +2,16 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task6 {
     public static double calculateS(double x) {
-        return (1 + (x * (((Math.pow(x, 2) *x) *x) / (2 * (3*2) * (4*3*2)))));
+        return (1 + (x * (((Math.pow(x, 2) *x) *x) / (factorial(2)*factorial(3)*factorial(4)))));
 
         }
 
+    private static int factorial(int n) {
+        for (int i = 1; i <= n; i++) {
+            n = n * i;
+            }
+            return n;
+    }
 
     public static double calculateZ(double x, double y) {
 
