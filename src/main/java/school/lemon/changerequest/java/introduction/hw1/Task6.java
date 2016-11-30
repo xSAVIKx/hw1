7df0fe2bd3c126f8a0c9e867ca2927ad627f9050
Tatyana Korchinova <tatyana.korchinova@gmail.com>
2016-11-30 18:51:14 +0400

    Tasks 6 and 9 are fixed.

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task6.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task6.java
@@ -17,7 +17,7 @@ public class Task6  {
 
     public static double calculateZ(double x, double y) {
         double z;
-        z = Math.sin(Math.pow(x, 3)) + Math.pow((Math.cos(y)), 2);
-        return z;}
+        return Math.sin(Math.pow(x, 3)) + Math.pow((Math.cos(y)), 2);
+    }
 
 }
--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task9.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task9.java
@@ -2,19 +2,11 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task9 {
 
-public static boolean isPowerOfThree(int n) {
-        while (n > 1 && (n % 3) == 0) {
-        n /=  3;
+        public static boolean isPowerOfThree(int n) {
+                while (n > 1 && (n % 3) == 0) {
+                        n /= 3;
+                }
+                return n == 1;
         }
-        if (n == 1) {
-        return true;
-        }
-        else return false;
-        }
-
-public static void main(String[] args) {
-        System.out.println(isPowerOfThree(15));
-        }
-
-        }
+}
 
