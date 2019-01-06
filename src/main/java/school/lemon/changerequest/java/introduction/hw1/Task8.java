362708ccbf4c2c9821fabcd5dde286e2630b977b
Yurii Serhiichuk <iurii.sergiichuk@gmail.com>
2019-01-06 14:22:34 +0200

    Use fastest possible solution

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task8.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task8.java
@@ -3,8 +3,8 @@ package school.lemon.changerequest.java.introduction.hw1;
 public class Task8 {
     public static int calculateSum(int N) {
         int result = 0;
-        for (int i = 0; i <= N; i++)
-            result += Math.pow(N + i, 2);
+        for (int i = N, sumLimit = N + N; i <= sumLimit; i++)
+            result += i * i;
         return result;
     }
 }
