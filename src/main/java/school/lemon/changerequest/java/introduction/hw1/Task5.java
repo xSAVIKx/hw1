8ce31671a098a2819b26900944d4b6833f68c452
Yurii Serhiichuk <iurii.sergiichuk@gmail.com>
2019-01-06 14:11:52 +0200

    Finish Task5

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task5.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task5.java
@@ -3,10 +3,15 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task5 {
     public static double calculateA(double x, double y, double z) {
-        return 0;
+        double topPart = 2 * Math.sin(x - Math.PI / 6) * calculateB(z);
+        double bottomPart = 1.0 / 2 + Math.pow(Math.sin(y), 2.0);
+        double a = topPart / bottomPart;
+        return a;
     }
 
     private static double calculateB(double z) {
-        return 0;
+        double sqrZ = z * z;
+        double b = 1 + sqrZ / (3 + sqrZ / 5);
+        return b;
     }
 }
