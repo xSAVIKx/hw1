1ed6630ba974162f56692669bb9adc6af5987597
Yurii Serhiichuk <iurii.sergiichuk@gmail.com>
2019-01-06 14:16:13 +0200

    Improve variables names

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task5.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task5.java
@@ -3,9 +3,9 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task5 {
     public static double calculateA(double x, double y, double z) {
-        double topPart = 2 * Math.sin(x - Math.PI / 6) * calculateB(z);
-        double bottomPart = 1.0 / 2 + Math.pow(Math.sin(y), 2.0);
-        double a = topPart / bottomPart;
+        double dividend = 2 * Math.sin(x - Math.PI / 6) * calculateB(z);
+        double divider = 0.5 + Math.pow(Math.sin(y), 2);
+        double a = dividend / divider;
         return a;
     }
 
