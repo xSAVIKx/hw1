5f1871f3bf86d54a22936b6404f61dba6669c96b
Yurii Serhiichuk <iurii.sergiichuk@gmail.com>
2019-01-06 14:14:18 +0200

    Finish Task8

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task8.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task8.java
@@ -2,6 +2,9 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task8 {
     public static int calculateSum(int N) {
-        return 0;
+        int result = 0;
+        for (int i = 0; i <= N; i++)
+            result += Math.pow(N + i, 2);
+        return result;
     }
 }
