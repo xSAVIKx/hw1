db931f7b990e6c3c6ebcef83e5495a177b11d51b
Yurii Serhiichuk <iurii.sergiichuk@gmail.com>
2019-01-06 14:09:34 +0200

    Finish Task3

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task3.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task3.java
@@ -2,6 +2,8 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task3 {
     public static boolean isBetween(int a, int b, int c) {
-        return false;
+        if ((a <= b) && (c >= b))
+            return true;
+        return (a >= b) && (c <= b);
     }
 }
