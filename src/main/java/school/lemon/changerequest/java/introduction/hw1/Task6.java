72cc3516614bd70c567334fe099bb64cabce4392
Yurii Serhiichuk <iurii.sergiichuk@gmail.com>
2019-01-06 14:13:18 +0200

    Finish Task6

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task6.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task6.java
@@ -2,10 +2,21 @@ package school.lemon.changerequest.java.introduction.hw1;
 
 public class Task6 {
     public static double calculateS(double x) {
-        return 0d;
+        double S = 1;
+        for (int i = 1; i <= 4; i++)
+            S += Math.pow(x, i) / factorial(i);
+        return S;
+    }
+
+    private static int factorial(int n) {
+        int res = 1;
+        for (int i = 2; i <= n; i++)
+            res *= i;
+        return res;
     }
 
     public static double calculateZ(double x, double y) {
-        return 0d;
+        double z = Math.sin(Math.pow(x, 3)) + Math.pow(Math.cos(y), 2);
+        return z;
     }
 }
